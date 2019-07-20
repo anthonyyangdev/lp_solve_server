637a15fa4299a30129d38384d45365533e19d724
Anthony Yang <ay339@cornell.edu>
2019-07-21 02:03:34 +0900

    Refresh project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import uuidv4 from 'uuid/v4';
-import shelljs from 'shelljs';
-import bodyParser from 'body-parser';
+require('dotenv/config');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = require('express')()
 
-const fs = require('fs');
-
-const app = express();
-app.use(cors());
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -18,22 +13,7 @@ app.use((req, res, next) => {
 });
 
 app.post('/', (req, res) => {
-  var content = req.body.code
-  var TEMP = `${uuidv4()}.lp`
-  fs.appendFile(TEMP, content, function (err) {
-    if (err) throw err;
-  });
-  shelljs.exec(`echo "${content}" >> ${TEMP}`)
-  var lp_solver = shelljs.exec(`lp_solve -S -S7 ${TEMP}`)
-
-  fs.unlink(TEMP, function (err) {
-    if (err) throw err;
-  });
-  res.send({
-    result: lp_solver,
-    status: lp_solver.code,
-    error: lp_solver.stderr
-  })
+  
 })
 
 app.listen(3000, () =>
