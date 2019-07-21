0ebe2143e2bae29e46a7cb18ba8b1a5a0ccac323
Anthony Yang <ay339@cornell.edu>
2019-07-21 20:06:31 +0900

    Prepare regex matching

--- src/Interpreter/eval.js
+++ src/Interpreter/eval.js
@@ -1,6 +1,45 @@
 
-function eval(line, model) {
+const TYPES = {
+  Statement: 1,
+  For: 2,
+  Iteration_Assign: 3,
+  Objective: 4,
+  Constraint: 5,
+  Name: 6,
+  Set: 7,
+  Assign: 8,
+  Expr: 9,
+  Sum: 10,
+  Term: 11,
+  Number: 12,
+  Variable: 13,
+  Relation_Constraint: 14,
+  Range_Constraint: 15,
+  Increasing: 16,
+  Decreasing: 17,
+  Relation: 18,
+  Integer: 19,
+  Float: 20,
+  Letter: 21,
+  Word: 22,
+  Subscript: 23,
+  LT: 24,
+  GT: 25,
+  EQ: 26
+}
+
+function getType(line) {
   
+}
+
+/**
+ * 
+ * @param {string} line 
+ * @param {Model} model 
+ * @returns {Model}
+ */
+function eval(line, model) {
+
 }
 
 
