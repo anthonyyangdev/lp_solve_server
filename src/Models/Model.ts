c290090133ca2ad136a40312e6d1c9101b344e74
Anthony Yang <ay339@cornell.edu>
2019-07-27 12:40:34 +0900

    Translate model into text

--- src/Models/Model.ts
+++ src/Models/Model.ts
@@ -5,6 +5,7 @@ import Environment from './Environment';
 import VariableType from './VariableTypes';
 import ObjectiveType from './ObjectiveType'
 import ConstraintModel from './ConstraintModel';
+import StringBuilder from 'src/StringBuilder/StringBuilder';
 
 class Constraints {
   private count: number = 0
@@ -21,6 +22,19 @@ class Constraints {
       expr: expression
     }
   }
+
+  public getConstraints() {
+    let count = 0
+    const arr = []
+    for (const name in this.map) {
+      arr[count++] = {
+        name,
+        expr: (this.map[name].expr as string)
+      }
+    }
+    return arr
+  }
+
 }
 
 class Objectives {
@@ -34,13 +48,18 @@ class Objectives {
     this.map = []
   }
 
-  public addConstraint(type: ObjectiveType, expr: string) {
+  public addObjective(type: ObjectiveType, expr: string) {
     const objectiveType = type === ObjectiveType.max ? 'max' : 'min'
     this.map[this.count++] = {
       opType: objectiveType,
       expr
     }
   }
+
+  public getObjectives() {
+    return this.map
+  }
+
 }
 
 class TypeDeclarations {
@@ -120,7 +139,7 @@ class Model {
 
   public addObjective(objective: ObjectiveModel) {
     const value = objective.getValues()
-    this.objective.addConstraint(value.name, value.expr)
+    this.objective.addObjective(value.name, value.expr)
   }
 
   public getObjective() {
@@ -134,9 +153,44 @@ class Model {
       this.types.add(type, v)
     }
   }
-
   public getAllTypeDeclarations() {
     return this.types.getAll()
+  }
+
+  public getModelText(): string {
+    let text = new StringBuilder()
+
+    const objectives = this.objective.getObjectives()
+    const constraints = this.constraints.getConstraints()
+    const types = this.types.getAll()
+    const ints = types.int
+    const frees = types.free
+    const bins = types.bin
+
+    for (const o of objectives) {
+      const objStatement = `${o.opType}: ${o.expr};\n`
+      text.append(objStatement)
+    }
+    for (const c of constraints) {
+      const conStatement = `${c.name}: ${c.expr};\n`
+      text.append(conStatement)
+    }
+
+    text.append(ints.length > 0 ? '' : 'int ')
+    for (let i = 0; i < ints.length; i++) {
+      text.append(i === ints.length - 1 ? `${ints[i]};\n` : `${ints[i]}, `)
+    }
+
+    text.append(frees.length > 0 ? '' : 'free ')
+    for (let i = 0; i < frees.length; i++) {
+      text.append(i === frees.length - 1 ? `${frees[i]};\n` : `${frees[i]}, `)
+    }
+
+    text.append(bins.length > 0 ? '' : 'bin ')
+    for (let i = 0; i < bins.length; i++) {
+      text.append(i === bins.length - 1 ? `${bins[i]};\n` : `${bins[i]}, `)
+    }
+    return text.toString()
   }
 
 }
