799bc07057299bdbb2d452832a3a17881aa6cef2
Anthony Yang <ay339@cornell.edu>
2019-11-26 21:01:28 -0500

    create parseProgram function

--- src/ParserRemodel/interpretToModel.ts
+++ src/ParserRemodel/interpretToModel.ts
@@ -1,13 +1,10 @@
-import removeComments from '../RemoveComments'
-import toStatements from '../ToArray/toArray'
+import parseProgram from './parseProgram'
 
 /**
  * @param {string} program The lp_solve program to be parsed and evaluated.
  */
 export default function interpretToModel(program: string) {
-  program = removeComments(program)
-  const statements = toStatements(program)
-  const programModel = parseStatements(statements)
+  const programModel = parseProgram(program)
   const evaluator = new Eval()
   return evaluator.eval(programModel)
 }
---
+++ src/ParserRemodel/parseProgram.ts
@@ -0,0 +1,382 @@
+import Tokenizer from './Tokenizer/Tokenizer'
+import TokenType from './Tokenizer/TokenType'
+import IterationModel from '../Models/IterationModel'
+import StringBuilder from '../StringBuilder/StringBuilder'
+import Token from './Tokenizer/Token'
+import Model from '../Models/Model'
+import SetModel from '../Models/SetModel';
+import ObjectiveModel from '../Models/ObjectiveModel';
+import TypeDeclareModel from '../Models/TypeDeclareModel';
+import VariableType from '../Models/VariableTypes';
+
+class Eval {
+  /**
+   * @param {Token} expected 
+   * @param {Token} actual
+   * @param {Tokenizer} stream 
+   */
+  private errorMsg(expected: TokenType, actual: Token, stream: Tokenizer) {
+    const positions = stream.getCurrentPosition()
+    const actualLiteral = actual.getLiteral()
+    const row = positions.line
+    const col = positions.column
+    return `Expected ${expected} but encountered ${actualLiteral} at line ${row}, word ${col}`
+  }
+
+  protected parseNumber(current: Token) {
+    const number = current.getLiteral().trim()
+    const regex = /^(([0-9]*\.[0-9]+)|([0-9]+\.?))$/
+    if (regex.test(number)) {
+      return number
+    }
+    else
+      throw new SyntaxError(`The number ${number} is not valid.`)
+  }
+
+  /**
+   * @param {Token} current 
+   */
+  protected parseVariable(current: Token) {
+    const word = current.getLiteral().trim()
+    const regex = /[a-zA-Z]\w*/
+    if (regex.test(word)) {
+      return word
+    }
+    else
+      throw new SyntaxError(`The variable name ${word} is not valid.`)
+  }
+
+  private parseOperator(current: Token) {
+    const operator = current.getLiteral().trim()
+    const regex = /[+-]/
+    if (regex.test(operator)) {
+      return operator
+    }
+    else
+      throw new SyntaxError(`The operator ${operator} is not valid.`)
+  }
+
+  protected parseExpression(stream: Tokenizer) {
+    let builder = new StringBuilder()
+    let end = false
+    while (!end && stream.hasNext()) {
+      let current = stream.peek()
+      const type = current.getType()
+      switch (type) {
+        case TokenType.Word:
+          const word = this.parseVariable(current)
+          builder.append(word)
+          break
+        case TokenType.Number:
+          const number = this.parseNumber(current)
+          builder.append(number)
+          break
+        case TokenType.MathOperator:
+          const operator = this.parseOperator(current)
+          builder.append(operator)
+          break
+        case TokenType.Sum:
+          const result = this.parseSum(stream)
+          builder.append(result.expr)
+          stream = result.stream
+          break
+        case TokenType.LPAREN:
+          stream.pop()
+          const inner = this.parseExpression(stream)
+          builder.append(inner.expr)
+          stream = inner.stream
+          break
+        case TokenType.RPAREN:
+        default:
+          end = true
+      }
+      if (!end) {
+        stream.pop()
+      }
+    }
+
+    const expr = builder.toString()
+    return {
+      expr,
+      stream
+    }
+  }
+
+  /**
+   * 
+   * @param {Tokenizer} TOKEN_STREAM
+   * @param {string}
+   */
+  protected parseSum(TOKEN_STREAM: Tokenizer) {
+    const expected = [
+      TokenType.Sum,
+      TokenType.LBRACKET,
+      TokenType.Word,
+      TokenType.Equal,
+      TokenType.Expr,
+      TokenType.To,
+      TokenType.Expr,
+      TokenType.RBRACKET,
+      TokenType.LPAREN,
+      TokenType.Expr,
+      TokenType.RPAREN,
+    ]
+
+    let sumModel = new IterationModel()
+    let expr = ''
+    for (const s of expected) {
+      if (!TOKEN_STREAM.hasNext())
+        throw new Error('There are no more tokens.')
+      let now = undefined
+      switch (s) {
+        case TokenType.Word:
+          now = TOKEN_STREAM.poll()
+          const word = this.parseVariable(now)
+          sumModel.addVariable(word)
+          break
+        case TokenType.Expr:
+          const { expr, stream } = this.parseExpression(TOKEN_STREAM)
+          sumModel.addExpr(expr)
+          TOKEN_STREAM = stream
+          break
+        default:
+          now = TOKEN_STREAM.poll()
+          if (now.getType() !== s)
+            throw new Error(this.errorMsg(s, now, TOKEN_STREAM))
+          break
+      }
+    }
+
+    // Transform sum model into an expression
+    return {
+      expr,
+      stream: TOKEN_STREAM,
+      sumModel
+    }
+
+  }
+
+  protected parseSet(stream: Tokenizer, model: Model) {
+    let expected = [
+      TokenType.Set,
+      TokenType.Word,
+      TokenType.Equal,
+      TokenType.Expr,
+    ]
+
+    const variable = new SetModel()
+    for (const s of expected) {
+      if (!stream.hasNext())
+        throw new Error('There are no more tokens.')
+      let now = undefined
+      switch (s) {
+        case TokenType.Word:
+          now = stream.poll()
+          const word = this.parseVariable(now)
+          variable.addName(word)
+          break
+        case TokenType.Expr:
+          const res = this.parseExpression(stream)
+          stream = res.stream
+          variable.addValue(res.expr)
+          break
+        default:
+          now = stream.poll()
+          if (now.getType() !== s)
+            throw new Error(this.errorMsg(s, now, stream))
+          break
+      }
+    }
+
+    const moreSetVariables = [variable]
+    expected = [
+      TokenType.Comma,
+      TokenType.Word,
+      TokenType.Equal,
+      TokenType.Expr,
+    ]
+    while (stream.peek().getType() !== TokenType.SemiColon) {
+      const setVar = new SetModel()
+      for (const s of expected) {
+        if (!stream.hasNext())
+          throw new Error('There are no more tokens.')
+        let now = undefined
+        switch (s) {
+          case TokenType.Word:
+            now = stream.poll()
+            const word = this.parseVariable(now)
+            setVar.addName(word)
+            break
+          case TokenType.Expr:
+            const res = this.parseExpression(stream)
+            stream = res.stream
+            setVar.addValue(res.expr)
+            break
+          default:
+            now = stream.poll()
+            if (now.getType() !== s)
+              throw new Error(this.errorMsg(s, now, stream))
+            break
+        }
+      }
+      moreSetVariables.push(setVar)
+    }
+
+    for (const v of moreSetVariables)
+      model.addSetVariable(v)
+
+    return model
+  }
+
+  /**
+   * @test Needs Testing
+   * @param stream 
+   * @param model 
+   */
+  protected parseObjective(stream: Tokenizer, model: Model) {
+    const expected = [
+      TokenType.Objective,
+      TokenType.SemiColon,
+      TokenType.Colon,
+      TokenType.Expr,
+    ]
+
+    const objective = new ObjectiveModel()
+    for (const s of expected) {
+      if (!stream.hasNext())
+        throw new Error('There are no more tokens.')
+      let now = undefined
+      switch (s) {
+        case TokenType.Objective:
+          now = stream.poll()
+          const opType = now.getLiteral()
+          objective.addObjective(opType)
+          break
+        case TokenType.Expr:
+          const res = this.parseExpression(stream)
+          stream = res.stream
+          objective.addExpression(res.expr)
+          break
+        default:
+          now = stream.poll()
+          if (now.getType() !== s)
+            throw new Error(this.errorMsg(s, now, stream))
+          break
+      }
+    }
+    model.addObjective(objective)
+    return model
+  }
+
+  /**
+   * Parses the next sequence of tokens, assuming that they form a type declaration
+   * statement. Parsing is complete when a semi-colon is detected in the token
+   * sequence. The information from the type declaration is added to the model.
+   * 
+   * @param stream Token stream
+   * @param model The optimization algebraic model
+   */
+  protected parseTypeDeclaration(stream: Tokenizer, model: Model) {
+
+    const typeDeclareModel = new TypeDeclareModel()
+
+    const checkPossible = (possible: Token, expected: TokenType, closure: any) => {
+      const actualType = possible.getType()
+      if (actualType === expected) {
+        closure()
+      } else {
+        throw new Error(`Was expecting a ${expected} but received ${actualType} instead.`)
+      }
+    }
+    let token = stream.poll()
+    checkPossible(token, TokenType.VariableType, () => {
+      typeDeclareModel.addType(VariableType.Int)
+    })
+    token = stream.poll()
+    checkPossible(token, TokenType.Word, () => {
+      const word = this.parseVariable(token)
+      typeDeclareModel.addVariable(word)
+    })
+
+    const expected = [
+      TokenType.Comma,
+      TokenType.Word
+    ]
+    while (stream.peek().getType() !== TokenType.SemiColon) {
+      for (const s of expected) {
+        if (!stream.hasNext())
+          throw new Error('There are no more tokens.')
+        let now = undefined
+        switch (s) {
+          case TokenType.Word:
+            now = stream.poll()
+            const name = this.parseVariable(now)
+            typeDeclareModel.addVariable(name)
+            break
+          default:
+            now = stream.poll()
+            if (now.getType() !== s)
+              throw new Error(this.errorMsg(s, now, stream))
+            break
+        }
+      }
+    }
+    model.addTypeDeclaration(typeDeclareModel)
+    return model
+  }
+
+  private parseToken(current: Token, model: Model, TOKEN_STREAM: Tokenizer) {
+    const current_type = current.getType()
+    switch (current_type) {
+      case TokenType.Sum:
+        const { expr, stream } = this.parseSum(TOKEN_STREAM)
+        TOKEN_STREAM = stream
+        // Do something with expression and add to model
+        break
+      case TokenType.Set:
+        model = this.parseSet(TOKEN_STREAM, model)
+        break
+      case TokenType.Objective:
+        model = this.parseObjective(TOKEN_STREAM, model)
+        break
+      case TokenType.VariableType:
+        model = this.parseTypeDeclaration(TOKEN_STREAM, model)
+        break
+    }
+    return {
+      model,
+      TOKEN_STREAM
+    }
+  }
+
+
+  /**
+   * @param {string} line 
+   * @param {Model} model 
+   * @returns {Model}
+   */
+  public eval(line: string, model: Model) {
+    let TOKEN_STREAM = new Tokenizer(line)
+    while (TOKEN_STREAM.hasNext()) {
+      const current = TOKEN_STREAM.peek()
+      const result = this.parseToken(current, model, TOKEN_STREAM)
+      model = result.model
+      TOKEN_STREAM = result.TOKEN_STREAM
+    }
+    return model
+  }
+}
+function parseStatement(statement: string) {
+  
+}
+
+import removeComments from '../RemoveComments'
+import toStatements from '../ToArray/toArray'
+
+export default function parseProgram(program: string) {
+  program = removeComments(program)
+  let statements = toStatements(program)
+  statements.map(s => {
+    return parseStatement(s)
+  })
+}
