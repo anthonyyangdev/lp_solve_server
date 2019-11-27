986c64411065293e40aa69987b48b98ee710fd98
Anthony Yang <ay339@cornell.edu>
2019-11-26 20:37:45 -0500

    Add documentation for toArray

--- src/ToArray/toArray.ts
+++ src/ToArray/toArray.ts
@@ -1,16 +1,24 @@
-function toArray(content: string) {
+/**
+ * Creates an array, where each element is a statement of an lp_solve program.
+ * The index of the array corresponds to the ordering of the statements.
+ * 
+ * @param program The program for lp_solve.
+ * @returns An array consisting of each statement of the program 
+ * in their original order.
+ */
+function toArray(program: string): Array<string> {
   const DELIMIT = ';'
 
-  let contentArray: string[] = content.split(DELIMIT)
+  let statements: string[] = program.split(DELIMIT)
     .map(x => x.trim())
-  const lastIndex: string = contentArray.pop() || ''
+  const lastIndex: string = statements.pop() || ''
   if (!/\s*/.test(lastIndex)) {
     throw new SyntaxError('Statement(s) did not end with semi-colon.')
   }
-  if (contentArray.some(x => x === '')) {
+  if (statements.some(x => x === '')) {
     throw new SyntaxError('Found statement with multiple semi-colons in a row.')
   }
-  return contentArray
+  return statements
 }
 
 export default toArray
